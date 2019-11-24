f7f57c4e2b2650b641907b57c74c7b91e3041646
NeamotUllah <52254369+NeamotUllah@users.noreply.github.com>
2019-11-24 23:45:49 +0600

    Add files via upload

---
+++ Neamot11609050/exerciseno1.php
@@ -0,0 +1,20 @@
+<?php
+
+
+echo "Twinkle, Twinkle little star.";
+echo '</br>';
+echo '</br>';
+
+$word1 = "Twinkle";
+$word2 = "star";
+
+
+echo "$word1, $word1 little $word2.";
+echo '</br>';
+echo '</br>';
+
+
+$word1 = "hello";
+$word2 = "hi";
+
+echo "$word1, $word1 little $word2.";
